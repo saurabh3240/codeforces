dd5a1ae050154bcc470d4ea3daf7f77eeb035fd4
saurabh3240 <jainsaurabh3240@gmail.com>
2018-02-26 16:14:56 +0530

    changed

--- 236.cpp
+++ 236.cpp
@@ -22,26 +22,26 @@
 #include <limits>
 #include <string>
 #include <cassert>
- 
+
 using namespace std;
 typedef long long LL;
 typedef pair<int,int> pii;
- 
+
 #define forup(i,a,b) for(int i=a; i<b; ++i)
 #define fordn(i,a,b) for(int i=a; i>b; --i)
 #define rep(i,a) for(int i=0; i<a; ++i)
- 
+
 #define dforup(i,a,b) for(i=a; i<b; ++i)
 #define dfordn(i,a,b) for(i=a; i>b; --i)
 #define drep(i,a) for(i=0; i<a; ++i)
- 
+
 #define slenn(s,n) for(n=0; s[n]!=13 and s[n]!=0; ++n);s[n]=0
- 
+
 #define gi(x) scanf("%d",&x)
 #define gl(x) scanf("%lld",&x)
 #define gd(x) scanf("%lf",&x)
 #define gs(x) scanf("%s",x)
- 
+
 #define pis(x) printf("%d ",x)
 #define pin(x) printf("%d\n",x)
 #define pls(x) printf("%lld ",x)
@@ -49,7 +49,7 @@ typedef pair<int,int> pii;
 #define pds(x) printf("%.12f ",x)
 #define pdn(x) printf("%.12f\n",x)
 #define pnl() printf("\n")
- 
+
 #define fs first
 #define sc second
 #define ll long long
@@ -79,7 +79,7 @@ ll gcd(ll a,ll b)
 	return a;
 }
 int main()
-{	
+{
 
 	string s;
 	cin>>s;
@@ -94,4 +94,3 @@ int main()
 		cout<<"CHAT WITH HER!";
 
 }
-
